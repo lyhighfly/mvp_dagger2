3dd9e321ef534b9528843caac0faade45643c771
liuyu35 <liuyu35@meituan.com>
2018-01-02 11:51:55 +0800

    新建工程，修改gitignore

---
+++ app/src/main/java/com/example/yuliu/mvp_dagger2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yuliu.mvp_dagger2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
